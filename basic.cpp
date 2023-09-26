713bf53d5b53c3a4800704790ff4ba5c1dd30994
rohit1836 <99047121+rohit1836@users.noreply.github.com>
2023-09-26 23:21:56 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,166 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    // cout << "Yeh duinya pittal di" << endl;
+    // cout << "Helo";
+
+
+
+
+    // ** Sum of two numbers **
+
+    // int a, b, sum;
+    // cout<<"Enter first number : ";
+    // cin>>a;
+    // cout<<"Enter second number : ";
+    // cin>>b;
+    // sum = a+b;
+    // cout<<"The sum is "<<sum;
+
+
+
+
+    // ** Calculate area of rectangle **
+
+    // float length, breadth, area;
+    // cout<<"ENter length : ";
+    // cin>>length;
+    // cout<<"Enter breadth : ";
+    // cin>>breadth;
+    // area = length * breadth;
+
+    // cout<<"Area of the rectangle is " <<area;
+
+
+
+
+
+    // ** Calculate area of circle **
+    
+    // float const pi = 3.14;
+    // float radius,area;
+    // cout<<"Enter radius : ";
+    // cin>>radius;
+    // area = pi * pow(radius,2);
+    // cout<<"Area of the circle is "<<area;
+
+
+
+
+    // ** Calculate volume of cylinder
+
+    // float const pi = 3.14;
+    // float radius,height,volume;
+    // cout<<"Enter radius : ";
+    // cin>>radius;
+    // cout<<"Enter height : ";
+    // cin>>height;
+
+    // volume = pi * pow(radius,2) * height;
+
+    // cout<<"Volume of the cylinder is "<<volume;
+
+
+
+
+
+
+    // ** Convert Celsius into fahrenheit ** 
+
+
+
+
+    // ** Calculate simple interest ** 
+    // float principal, time, rate, si;
+    // cout<<"Enter principal amount : ";
+    // cin>>principal;
+    // cout<<"Enter time period : ";
+    // cin>>time;
+    // cout<<"enter rate percent : ";
+    // cin>>rate;
+
+    // si = (principal * rate * time)/100;
+    
+    // cout<<"The intrest is "<<si;
+
+
+
+
+
+
+
+    // Check whether entered year is leap year or not
+    // int year;
+    // cout<<"Enter year : ";
+    // cin>>year;
+
+    // if (year%4==0)
+    // {
+    //     cout<<"Its a leap year!!";
+    // }
+    // else{
+    //     cout<<"Its a normal year";
+    // }
+    
+
+
+
+
+
+
+    // Check whether the character entered by the user is lowercase or uppercase
+
+    // char c;
+    // cout<<"Enter a character : ";
+    // cin>>c;
+
+    // if (c)
+    // {
+    //     /* code */
+    // }
+    
+
+
+
+
+
+
+
+    // Find the greatest of four numbers entered by the user
+
+    int numbers[4];
+    
+    string usr = "Enter number : ";
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<usr<<"\n";
+        cin>>numbers[i];
+    }
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     if (/* condition */)
+    //     {
+    //         /* code */
+    //     }
+        
+    // }
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
